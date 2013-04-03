de654c893266703b09661bd62bd3ed9d145b7ee4
vahab <vhbsoft@gmail.com>
2013-04-03 14:43:44 -0700

    moved source files to sender and receiver folder

---
+++ src/receiver/PreExperimentTCPReceiver.c
@@ -0,0 +1,87 @@
+#include "stdio.h"      /* for printf() and fprintf() */
+#include "sys/socket.h" /* for socket(), bind(), connect(), send(), recv() */
+#include "arpa/inet.h"  /* for sockaddr_in, inet_addr(), inet_ntoa() */
+#include "stdlib.h"     /* for atoi() and exit() */
+#include "string.h"     /* for memset() */
+#include "unistd.h"     /* for close() */
+
+#include "unitExperiment.h" /*Contains all Experiment Functions*/
+
+
+/*SERVER PORT*/
+#define SERVER_PORT 16400
+
+enum error_t PreExperimentTCPReceiver(int* num_of_packets,
+                      unsigned long* inter_packet_departure_spacing,
+                      int* probe_packet_length){
+
+  /*Declertion of Constants*/
+  const size_t INTEGER_SIZE=sizeof(int);       /*Length of integer*/
+  const size_t LONG_SIZE=sizeof(unsigned long);/*Length of Long*/
+
+  /* Length of send buffer holding int, unsigned long, int */
+  const unsigned int BUFFER_REC_LENGTH=INTEGER_SIZE*2+LONG_SIZE; 
+
+  /*Variable Creation and Initializaiton*/
+  int servSock;                      /* Socket descriptor for Server */
+  int clntSock;                      /* Socket descriptor for Client */
+  struct sockaddr_in servAddr;       /* Server address */
+  struct sockaddr_in clntAddr;       /* Client address */
+  char recBuffer[BUFFER_REC_LENGTH]; /* Buffer for receiving packets */
+  size_t clntLen = sizeof(clntAddr); /* Set the size of the in-out parameter */
+
+  /*Initiate a TCP connection*/
+  /* Create a reliable, stream socket using TCP */
+  if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  {
+    fprintf(stderr, "ERROR #%d: Socket Setup Error", SOCKET_SETUP_ERROR);
+    return SOCKET_SETUP_ERROR;
+  }
+  
+  /* Construct the server address structure */
+  memset(&servAddr, 0, sizeof(servAddr));         /* Zero out structure */
+  servAddr.sin_family      = AF_INET;             /* Internet address family*/
+  servAddr.sin_addr.s_addr = htonl(INADDR_ANY);   /* Server IP address */
+  servAddr.sin_port        = htons(SERVER_PORT);     /* Server port */
+  
+  /* Bind to the local address */
+  if (bind(servSock,(struct sockaddr *)&servAddr,sizeof(servAddr)) < 0)
+  {
+    fprintf(stderr, "ERROR #%d: Binding Error", BIND_ERROR);
+    return BIND_ERROR;
+  }
+  
+  /* Mark the socket so it will listen for incoming connections */
+  if (listen(servSock, 5) < 0)
+  {
+    fprintf(stderr, "ERROR #%d: Port Listening Error", LISTEN_ERROR);
+    return LISTEN_ERROR;
+  }
+  
+  /* Wait for a client to connect */
+  if ((clntSock = accept(servSock,(struct sockaddr*)&clntAddr,&clntLen)) < 0)
+  {
+    fprintf(stderr, "ERROR #%d: Accept Client Error", ACCEPT_CLIENT_ERROR);
+    return ACCEPT_CLIENT_ERROR;
+  }
+  
+  /* Receive Data */
+  if (recv(clntSock, recBuffer, BUFFER_REC_LENGTH , 0) <= 0)
+  {
+    fprintf(stderr, "ERROR #%d: Receive Data Error", RECEIVE_ERROR);
+    return RECEIVE_ERROR;
+  }
+  
+ /*Extract Data from recBuffer and put into appropriate variables*/                                    
+  memcpy(num_of_packets, (recBuffer), INTEGER_SIZE);
+  memcpy(inter_packet_departure_spacing, (recBuffer+INTEGER_SIZE), LONG_SIZE);
+  memcpy(probe_packet_length, (recBuffer+INTEGER_SIZE+LONG_SIZE), INTEGER_SIZE);
+
+  /*Close Socket*/
+  close(servSock);
+
+  return 0;
+
+}
+
+
---
+++ src/sender/unitExperimentSender.c
@@ -0,0 +1,57 @@
+/*
+Usage:
+./unitExperimentSender --numPackets1000 --interPacketDepartureSpacing10 --probePacketLength1500 --logFile”~/log/experiment1” --destAddr”131.179.192.201” --compressionNodeAddr”131.179.192.202” --timeout10 --entropy’L’
+
+Example:
+./unitExperimentSender 1000 10 1500 ”~/log/experiment1” ”131.179.192.201” “131.179.192.202” 10 ‘L’
+*/
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#include "unitExperiment.h" /*Contains all Experiment Functions*/
+
+int main(int argc, char *argv[])
+{
+
+  int                num_of_packets = atoi(argv[1]);           //Number of Packets [1,60000]
+  unsigned long      inter_packet_departure_spacing = strtoul(argv[2],0,0);  //[0,10^9]    in nano-seconds
+  int                probe_packet_length = atoi(argv[3]);   //[0,1500]    in bytes
+  char*              experiment_run_log_absolute_filename = argv[4];    //FILENAME_SIZE = 255
+  char*              dest_addr = argv[5];
+  char*              compression_node_addr = argv[6];
+  unsigned long      packet_train_timeout = strtoul(argv[7],0,0);     //[0,10^6]     in msec
+  char*              entropy = argv[8]; 
+  int* timeout_estimation;
+
+  /*Call initial TCP Connection to Set up receiver*/
+  if(PreExperimentTCPConnection(num_of_packets,inter_packet_departure_spacing,
+                      probe_packet_length,timeout_estimation,dest_addr) != 0)
+  {
+    fprintf(stderr, "ERROR #%d: PreExperiment TCP Sender Error", PRE_EXPERIMENT_TCP_SENDER_FAILED);
+    return PRE_EXPERIMENT_TCP_SENDER_FAILED;                    
+  }
+
+  /*Call UDP Connection to Send Data to Receiver*/
+  if(UDPTrainGenerator(num_of_packets,inter_packet_departure_spacing,  
+                      probe_packet_length,entropy,compression_node_addr) != 0)
+  {
+    fprintf(stderr, "ERROR #%d: UDP Train Generator Error", UDP_TRAIN_GENERATOR_FAILED);
+    return UDP_TRAIN_GENERATOR_FAILED;                    
+  }
+
+  /*CREATE WAIT TIMEOUT FUNCTION*/
+  sleep(*timeout_estimation);
+
+  /*Call Final TCP Connection to receive packets*/
+  if(PostExperimentTCPConnection(experiment_run_log_absolute_filename, dest_addr) != 0)
+  {
+    fprintf(stderr, "ERROR #%d: PostExperiment TCP Sender Error", POST_EXPERIMENT_TCP_SENDER_FAILED);
+    return POST_EXPERIMENT_TCP_SENDER_FAILED;                    
+  }
+
+}
+
+
