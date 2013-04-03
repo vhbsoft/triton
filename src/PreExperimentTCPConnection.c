7774067a4fe8c64b4e2c6cf9c404c5214def8a34
vahab <vhbsoft@gmail.com>
2013-04-03 14:38:18 -0700

    added UDP errors and usage

--- src/PreExperimentTCPConnection.c
+++ src/PreExperimentTCPConnection.c
@@ -1,10 +1,11 @@
-#include "unitExperiment.h" /*Contains all Experiment Functions*/
 #include "stdio.h"      /* for printf() and fprintf() */
 #include "sys/socket.h" /* for socket(), bind(), connect(), send(), recv() */
 #include "arpa/inet.h"  /* for sockaddr_in, inet_addr(), inet_ntoa() */
 #include "stdlib.h"     /* for atoi() and exit() */
 #include "string.h"     /* for memset() */
 #include "unistd.h"     /* for close() */
+
+#include "unitExperiment.h" /*Contains all Experiment Functions*/
 
 /*SERVER PORT*/
 #define SERVER_PORT 16400
@@ -14,44 +15,59 @@ enum error_t PreExperimentTCPConnection(int num_of_packets,
                       int probe_packet_length,
                       int* timeout_estimation,
                       char* dest_addr){
+
   /*Declertion of Constants*/
   const size_t INTEGER_SIZE=sizeof(int);     /*Length of integer */
   const size_t LONG_SIZE=sizeof(unsigned long);/*Length of Long*/
+
   /* Length of send buffer holding int, unsigned long, int */
   const unsigned int BUFFER_SEND_LENGTH=INTEGER_SIZE*2+LONG_SIZE;
   const int IP_ADDRESS_BUFFER_LENGTH = 16;   /*length of buffer for IP Address*/
+
   /*Variable Creation and Initializaiton*/
   int sock;                                  /* Socket descriptor */
   struct sockaddr_in servAddr;               /* Server address */
   char sendBuffer[BUFFER_SEND_LENGTH];       /* Buffer for sending packets */
   char servIP[IP_ADDRESS_BUFFER_LENGTH];     /* Server IP address (dotted quad) */
   strcpy( servIP, dest_addr);                /*Initialize the servIP with Destination Address*/
+
   /*fill sendBuffer with data*/                                    
   memcpy((sendBuffer), &num_of_packets, INTEGER_SIZE);
   memcpy((sendBuffer+INTEGER_SIZE), &inter_packet_departure_spacing, LONG_SIZE);
   memcpy((sendBuffer+INTEGER_SIZE+LONG_SIZE), &probe_packet_length, INTEGER_SIZE);
+
   /*Initiate a TCP connection*/
   /* Create a reliable, stream socket using TCP */
-  if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
+  if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  {
     fprintf(stderr, "ERROR #%d: Socket Setup Error", SOCKET_SETUP_ERROR);
     return SOCKET_SETUP_ERROR;
   }
+  
   /* Construct the server address structure */
   memset(&servAddr, 0, sizeof(servAddr));         /* Zero out structure */
   servAddr.sin_family      = AF_INET;             /* Internet address family*/
   servAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
   servAddr.sin_port        = htons(SERVER_PORT);     /* Server port */
+
   /* Establish the connection to the echo server */
-  if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0){
+  if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
+  {
     fprintf(stderr, "ERROR #%d: Connection Setup Error", CONNECT_ERROR);
     return CONNECT_ERROR;
   }
+
   /* Send the data to the server */
   if (send(sock, sendBuffer, BUFFER_SEND_LENGTH, 0) != BUFFER_SEND_LENGTH){
     fprintf(stderr, "ERROR #%d: Send Data Error", SEND_ERROR);
     return SEND_ERROR;
   }
+
   /*Close Socket*/
   close(sock);
-  return 0;
+ 
+ return 0;
+
 }
+
+
--- src/unitExperimentReceiver.c
+++ src/unitExperimentReceiver.c
@@ -1,11 +1,14 @@
-// TODO add full description of usage here
-// ./unitExperiment 1000 10 1500 ”~/log/experiment1” ”131.179.192.201” “131.179.192.202” 10 ‘L’
+/*
+Usage: 
+./unitExperimentReceiver
+*/
 
-#include "unitExperiment.h" /*Contains all Experiment Functions*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <signal.h>
+
+#include "unitExperiment.h" /*Contains all Experiment Functions*/
 
 struct thread_data
 {
@@ -14,14 +17,16 @@ struct thread_data
         int 		   send_buffer_length;
 };
 
-char send_buffer[150000];    //Hardcoded based on max number of probe packets in each experiment = 6000
+char send_buffer[150000]; //Hardcoded based on max number of probe packets in each experiment = 6000
 int send_buffer_length = 0;
 
 void w_UDPTrainGenerator(struct thread_data* td)
 {
-	if(UDPTrainReceiver(td->send_buffer, td->send_buffer_length, td->probe_packet_length) != 0){
-    	exit(-1);
-  	}
+	if(UDPTrainReceiver(td->send_buffer, td->send_buffer_length, td->probe_packet_length) != 0)
+	{
+		fprintf(stderr, "ERROR #%d: UDP Train Receiver Error", UDP_TRAIN_RECEIVER_FAILED);
+    		return UDP_TRAIN_RECEIVER_FAILED;  	
+	}
 
 }
 
@@ -37,10 +42,8 @@ int main(int argc, char *argv[])
   if(PreExperimentTCPReceiver(&num_of_packets, &inter_packet_departure_spacing, 
                               &probe_packet_length) != 0)
   {
-  
     fprintf(stderr, "ERROR #%d: PreExperiment TCP Receiver Error", PRE_EXPERIMENT_TCP_RECEIVER_FAILED);
     return PRE_EXPERIMENT_TCP_RECEIVER_FAILED;                    
-  
   }
 
 	pthread_t t0;
@@ -54,16 +57,16 @@ int main(int argc, char *argv[])
   /*Call initial TCP Connection to Set up receiver*/
   if(PostExperimentTCPReceiver(send_buffer, send_buffer_length) != 0)
   {
-
     fprintf(stderr, "ERROR #%d: PostExperiment TCP Receiver Error", POST_EXPERIMENT_TCP_RECEIVER_FAILED);
     return POST_EXPERIMENT_TCP_RECEIVER_FAILED;
-
   }
 
 	pthread_kill(t0,SIGINT);
 
 	pthread_join(t0, NULL);
+
 	return 0;
+
 }
 
 
