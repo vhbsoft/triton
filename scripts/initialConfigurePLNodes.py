03502f8c53eab2881278557da24a7009efb958d9
mfouladi <fouladimatin@gmail.com>
2013-04-24 17:46:01 -0700

    Updated Installing Python Script: errors fixed

--- scripts/initialConfigurePLNodes.py
+++ scripts/initialConfigurePLNodes.py
@@ -13,17 +13,25 @@
 ################################
 #setup_version = 1.0
 #skip for now
+import paramiko
+import cmd
+import time
+import sys
 
 #Commands can be issued remotely using ssh in the following format:
 #ssh ucla_triton@[planetlab_hostname] "[list of commands]" (e.g.,"ls -l; ps -aux; whoami")
-import paramiko
+planetlab_hostname = sys.argv[1]
 
-#for every_line_in_file set planetlab_hostname = this_line until end_of_file
+#open file of planetlab host names
+#planetlab_host_file = sys.argv[1]
+#planetlab_host_file = open(planetlab_host_file, 'r')
 
-planetlab_hostname = ""
+#set global variables for username and password
 planetlab_username = "ucla_triton"
 planetlab_password = "uclaucla"
 
+#for every_line_in_file set planetlab_hostname = this_line until end_of_file
+#for planetlab_hostname in planetlab_host_file.readlines():
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(planetlab_hostname, username=planetlab_username, password= planetlab_password)
@@ -33,8 +41,13 @@ ssh.connect(planetlab_hostname, username=planetlab_username, password= planetlab
 ################################
 #zlib-devel.i386 is for compression modules
 #sudo yum groupinstall "Development Tools"
-ssh.exec_command("sudo yum -y install man gzip less gcc flex bison bc rpm-build make iperf zlib-devel.i386 git")
-ssh.write(PlanetLabPassword)
+stdin, stdout, stderr = ssh.exec_command("sudo yum -y install man gzip less gcc flex bison bc rpm-build make iperf zlib-devel.i386 git")
+stdin.write(planetlab_password)
+stdin.flush()
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
 
 #Install pathrate (skip for now)
 
@@ -46,9 +59,21 @@ ssh.write(PlanetLabPassword)
 #mkdir ~/triton_1_0
 #mkdir ~/triton_results
 #mkdir ~/triton_logs
-ssh.exec_command("mkdir ~/triton_1_0")
-ssh.exec_command("mkdir ~/triton_results")
-ssh.exec_command("mkdir ~/triton_logs")
+stdin,stdout,stderr = ssh.exec_command("mkdir ~/triton_1_0")
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+stdin,stdout,stderr = ssh.exec_command("mkdir ~/triton_results")
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+stdin,stdout,stderr = ssh.exec_command("mkdir ~/triton_logs")
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
 
 ###########################################
 #Copy executables or source files
@@ -56,14 +81,68 @@ ssh.exec_command("mkdir ~/triton_logs")
 #use the github repository
 #git clone inside ~/triton_1_0
 #config files are also assumed to be included in the git repository
-ssh.exec_command("git clone https://github.com/vhbsoft/triton.git ~triton_1_0")
+
+print "Initializing Git Folder\n"
+stdin,stdout,stderr = ssh.exec_command("cd ~/triton_1_0/; git clone git://github.com/vhbsoft/triton.git; cd ~/ ;")
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+time.sleep(1)
+
+#if git folder already exists, then update it
+print "Updating Git Folder\n"
+stdin,stdout,stderr = ssh.exec_command("cd ~/triton_1_0/triton/; git pull origin master; cd ~/ ;")
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+time.sleep(1)
 
 ###########################################
 #Compile the source code
 ###########################################
 #run 'make' in every single directory (sender/receiver/compression/decompression)
-ssh.exec_command("make -C ~/triton_1_0/src/sender")
-ssh.exec_command("make -C ~/triton_1_0/src/receiver")
-ssh.exec_command("make -C ~/triton_1_0/src/compression")
-ssh.exec_command("make -C ~/triton_1_0/src/decompression")
+print "Compiling Sender \n"
+stdin,stdout,stderr = ssh.exec_command("cd ~/triton_1_0/triton/src/sender/; sudo make;")
+stdin.write(planetlab_password)
+stdin.flush()
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+#print "finished make1\n"
+time.sleep(1)
+
+print "Compiling Receiver \n"
+stdin,stdout,stderr = ssh.exec_command("cd ~/triton_1_0/triton/src/receiver/; sudo make;")
+stdin.write(planetlab_password)
+stdin.flush()
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+#print "finished make2\n"
+time.sleep(1)
+
+print "Compiling Compression \n"
+stdin,stdout,stderr = ssh.exec_command("cd ~/triton_1_0/triton/src/compression/; sudo make;")
+stdin.write(planetlab_password)
+stdin.flush()
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+#print "finished make3\n"
+time.sleep(1)
+
+print "Compiling Decompression \n"
+stdin,stdout,stderr = ssh.exec_command("cd ~/triton_1_0/triton/src/decompression/; sudo make;")
+stdin.write(planetlab_password)
+stdin.flush()
+for line in stdout.read().splitlines():
+	print '%s' % line
+for line in stderr.read().splitlines():
+	print '%s' % line
+time.sleep(1)
 
